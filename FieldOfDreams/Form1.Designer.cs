f2f0681751961442bbb232ee4370c2c9dd58314f
aktec666 <aktec@mail.ru>
2021-11-02 10:39:56 +0300

    Initial commit

---
+++ FieldOfDreams/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace FieldOfDreams
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelWord = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // labelWord
+            // 
+            this.labelWord.AutoSize = true;
+            this.labelWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelWord.Location = new System.Drawing.Point(200, 29);
+            this.labelWord.Name = "labelWord";
+            this.labelWord.Size = new System.Drawing.Size(249, 91);
+            this.labelWord.TabIndex = 0;
+            this.labelWord.Text = "label1";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(117, 146);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(433, 213);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(870, 452);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.labelWord);
+            this.Name = "Form1";
+            this.Text = "Поле чудес";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelWord;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ FieldOfDreams/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace FieldOfDreams
+{
+    public partial class Form1 : Form
+    {
+        string SecretWord = "Программист";
+        string MaskWord = "";
+        int Count = 10;
+
+        void GenerateButtons()
+        {
+            for (char i ='А'; i <= 'Я'; i++)
+            {
+                Button X = new Button();
+                X.Width = 30;
+                X.Height = 30;
+                X.Text += i;
+                X.BackColor = Color.Cyan;
+                X.Click += Letter_Click;
+                flowLayoutPanel1.Controls.Add(X);
+            }
+        }
+
+        private void Letter_Click(object sender, EventArgs e)
+        {
+            (sender as Button).BackColor = Color.Fuchsia;
+
+
+            if (EndGame())
+                MessageBox.Show("Конец игры");
+            else
+            {
+                string result = SearchAndReplace(SecretWord, (sender as Button).Text, MaskWord);
+                if (result == "error")
+                    MessageBox.Show("Ошибка");
+                else
+                {
+                    MaskWord = result;
+                    labelWord.Text = result;
+                    if (result == SecretWord)
+                    {
+                        MessageBox.Show("Победа");
+                        Count = 0;
+                    }
+                }
+                Count--;
+            }
+        }
+
+
+            public Form1()
+        {
+            InitializeComponent();
+            GenerateButtons();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SecretWord = SecretWord.ToUpper();
+
+            for (int i = 0; i < SecretWord.Length; i++)
+                MaskWord += "*";
+            labelWord.Text = MaskWord;
+
+        }
+
+        private string SearchAndReplace(string secret, string input, string mask)
+        {
+
+            if (input.Length == 1 && input[0] >= 'А' && input[0] <= 'Я')
+            {
+                string temp = "";
+                for (int i = 0; i < secret.Length; i++)
+                    if (secret[i] == input[0])
+                        temp += input[0];
+                    else
+                        temp += mask[i];
+                return temp;
+            }
+            else
+                return "error";
+
+        }
+
+        private bool EndGame()
+        {
+            return Count == 0;
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
